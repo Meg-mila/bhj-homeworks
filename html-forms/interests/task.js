c807cc0b14e5f8430fe1f32f8e3e2ec64f878677
Margarita Perepecho <meg.mila27@gmail.com>
2021-01-19 13:20:03 +0000

    add task interest

---
+++ html-forms/interests/task.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const interestCheck = document.querySelectorAll( '.interest__check' );
+
+for (let i = 0; i < interestCheck.length; i++) {
+    if (interestCheck[i].closest('.interest').querySelector('.interests_active')) {
+        interestCheck[i].addEventListener('change', () => {
+            let interestCheckBox = interestCheck[i].closest('.interest').querySelector('.interests_active').querySelectorAll('.interest__check');
+            for (let j = 0; j < interestCheckBox.length; j++) {
+                if (interestCheck[i].checked) {
+                    interestCheckBox[j].checked = true;
+                } else {
+                    interestCheckBox[j].checked = false;
+                }
+            }
+        })
+    }
+};
