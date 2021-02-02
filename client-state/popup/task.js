ae43e368ad183377b5e908dc3a63f050cee622f3
Margarita Perepecho <meg.mila27@gmail.com>
2021-02-02 13:00:29 +0000

    change popup task

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -3,18 +3,16 @@
 const subscribe = document.getElementById('subscribe-modal');
 const modalClose = document.querySelector('.modal__close');
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (!getCookie('modal=closed')) {
-        subscribe.classList.add('modal_active');
-    }
-});
-
-function getCookie(name) {
-    let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
+if (!getCookie('modal')) {
+    subscribe.classList.add('modal_active');
 }
 
 modalClose.addEventListener('click', () => {
     document.cookie = 'modal=closed';
     subscribe.classList.remove('modal_active');
 });
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
